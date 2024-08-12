6b25991bff83670c18ed255f7273384b8ade6fd4
GuilhermeHagos <63023398+GuilhermeHagos@users.noreply.github.com>
2024-08-12 13:50:45 -0300

    Desenvolvendo metódo para coletar audio features de uma playlist

--- modelo_recomendacao_spotify.py
+++ modelo_recomendacao_spotify.py
@@ -4,19 +4,58 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
+import pandas as pd
 
 # %%
-load_dotenv()
+# Carregando arquivo contendo variaveis de ambiente
+dotenv_path = 'E:\Programinhas\GitHub_Projects\Sistemas de recomendação com Spotify\Variaveis_ambiente.env'
+load_dotenv(dotenv_path=dotenv_path)
 
+# %%
 #Acesso as variaveis de ambiente
-CLIENT_ID= os.getenv('CLIENT_ID')
-CLIENT_SECRET=os.getenv('CLIENT_SECRET')
-REDIRECT_URI=os.getenv('REDIRECT_URI')
+SPOTIPY_CLIENT_ID= os.getenv('SPOTIPY_CLIENT_ID')
+SPOTIPY_CLIENT_SECRET=os.getenv('SPOTIPY_CLIENT_SECRET')
+SPOTIPY_REDIRECT_URI=os.getenv('SPOTIPY_REDIRECT_URI')
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth)(
-    client_id = CLIENT_ID,
-    client_secret = CLIENT_SECRET,
-    redirect_uri = REDIRECT_URI,
+
+# %%
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id = SPOTIPY_CLIENT_ID,
+    client_secret = SPOTIPY_CLIENT_SECRET,
+    redirect_uri = SPOTIPY_REDIRECT_URI,
     # alterei scope por entender qque o abaixo se adequa as ideias do projeto de contruir playlist através de outras
     scope = 'user-library-modify playlist-modify-public'
-)
+))
+
+
+# %%
+
+def get_audio_features_by_playlist(playlist_id):
+    '''
+    Função recebe como parâmetro o ID de uma playlist e retorna Dataframe contendo as features de audio das músicas contidas na playlist
+    '''
+    ## Buscando todas as músicas de uma playlist ( playlist_id) e associando a results
+    results = sp.playlist_tracks(playlist_id)
+    # as musicas são então adicionadas a lista tracks
+    tracks = results['items']
+    # Pelo tamanho da playlist, pode ser que a API do spotify venha paginado
+    # então o while results garante que se houver, ele vai adicionar as músicas a playlist
+    while results['next']:
+        results = sp.next(results)
+        tracks.extend(results['items'])
+    
+    # Lista vazia contendo as features das musicas é iniciada e através do sp.audio_features é feito a coleta das características de audio de cada música
+
+    features = []
+    for item in tracks:
+        track_id = item['track']['id']
+        features = sp.audio_features(track_id)[0]
+        if features:
+            features.append(features)
+    return pd.DataFrame(features)
+# %%
+minha_playlist_id = '1U4UVF4hG7C7UZafH1nITe'
+df = get_audio_features_by_playlist(minha_playlist_id)
+df.head(10)
+
+# %%
